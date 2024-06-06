3cb45a637843b2855afed1c6411942c79a8586d1
Cano <41741040+canoypa@users.noreply.github.com>
2024-06-06 20:55:59 +0900

    scheduled

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,23 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+const scheduledTask = async (
+  _controller: ScheduledController,
+  _env: unknown,
+  _c: ExecutionContext,
+) => {
+  // TODO
+}
+
+const scheduled: ExportedHandlerScheduledHandler = async (
+  controller,
+  env,
+  c,
+) => {
+  c.waitUntil(scheduledTask(controller, env, c))
+}
+
+export default {
+  fetch: app.fetch,
+  scheduled,
+}
