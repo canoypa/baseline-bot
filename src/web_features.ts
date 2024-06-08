aa1cac77eee8cfec1737d2ce0d9aaff0d4488fb4
Cano <41741040+canoypa@users.noreply.github.com>
2024-06-08 19:30:34 +0900

    web-features

---
+++ src/web_features.ts
@@ -0,0 +1,50 @@
+/**
+ * @license https://github.com/web-platform-dx/web-features/blob/main/LICENSE.txt
+ */
+
+export type BaselineIdentifier = 'high' | 'low'
+
+export type BrowserIdentifier =
+  | 'chrome'
+  | 'chrome_android'
+  | 'edge'
+  | 'firefox'
+  | 'firefox_android'
+  | 'safari'
+  | 'safari_ios'
+
+export type SupportBrowser = {
+  [K in BrowserIdentifier]?: string
+}
+
+export type SupportStatus = {
+  baseline: BaselineIdentifier | false
+  baseline_low_date?: string
+  baseline_high_date?: string
+  support: SupportBrowser
+}
+
+export type WebFeature = {
+  name: string
+  description: string
+  description_html: string
+  status: SupportStatus
+
+  spec: string | string[]
+  alias?: string | string[]
+  caniuse?: string | string[]
+  compat_features?: string[]
+  usage_stats?: string | string[]
+}
+
+export type WebFeatures = {
+  [id: string]: WebFeature
+}
+
+export const fetchWebFeatures = async () => {
+  const res = await fetch(
+    'https://www.unpkg.com/web-features@latest/index.json',
+  ).then((r) => r.json() as Promise<WebFeatures>)
+
+  return res
+}
